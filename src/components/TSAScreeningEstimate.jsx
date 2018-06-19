172397fa98bfbafc38af72d6867b47b5da7dc808
Cameron <cameron.grams.2@gmail.com>
2018-06-19 16:14:38 -0400

    working model, adjust the number of times delay displayed for multiple incidents

--- src/components/TSAScreeningEstimate.jsx
+++ src/components/TSAScreeningEstimate.jsx
@@ -1,7 +1,7 @@
 import React from 'react'; 
 import Aux from '../util/Aux'; 
 import { securityCheckpointDelay } from '../util/checkpointAPI'; 
-// import Waiting from './Loader/Loader'; 
+import Waiting from './Loader/Loader'; 
 
 
 class TSAEstimate extends React.Component{
@@ -31,7 +31,9 @@ class TSAEstimate extends React.Component{
     }
 
     render(){
-       let estimateDisplay = <p className={ this.state.lengthOfWait } >TSA Estimated Time for Screening: { this.state.reportedWait } </p>;
+
+        let estimateDisplay =  this.state.isLoading ? <Waiting /> : 
+            <p className={ this.state.lengthOfWait } >TSA Estimated Time for Screening: { this.state.reportedWait } </p>;
 
         return(
             <Aux>{ estimateDisplay }</Aux>
@@ -40,11 +42,3 @@ class TSAEstimate extends React.Component{
 };
 
 export default TSAEstimate;
-
-
-/*
-         let estimateDisplay =  <Aux>{ this.state.isLoading && <Waiting /> }</Aux> || 
-            <p className={ this.state.lengthOfWait } >TSA Estimated Time for Screening: { this.state.timeRequired } </p>;
-       
- 
-*/
